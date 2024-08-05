a0f59cc8ec6ece541d9e0a2a17e783c7f646ac12
aboukhatem <166718968+aboukhatem@users.noreply.github.com>
2024-08-05 08:59:29 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 test
 
   title: Customers v2
-  description: "Customers v2 API definition"
+  description: "Customers v22 API definition"
   version: 2.8.0
 host: iw5rcegg16-vpce-070cc092dff107cba.execute-api.eu-west-1.amazonaws.com
 basePath: /
